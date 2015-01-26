2cda23d54862fb6593b5b6961ac80beeee2abb2e
dsdshcym <dsdshcym@gmail.com>
2015-01-26 13:13:44 +0800

    Complete latency(Lesson 4 Problem set(optional) 2)

---
+++ codes/latency.py
@@ -0,0 +1,19 @@
+# Write a procedure, speed_fraction, which takes as its inputs the result of
+# a traceroute (in ms) and distance (in km) between two points. It should
+# return the speed the data travels as a decimal fraction of the speed of
+# light.
+
+speed_of_light = 300000. # km per second
+
+def speed_fraction(tracert, distance):
+    speed_of_data = distance * 2.0 / tracert * 1000.0
+    return speed_of_data / speed_of_light
+
+print speed_fraction(50,5000)
+#>>> 0.666666666667
+
+print speed_fraction(50,10000)
+# >>> 1.33333333333  # Any thoughts about this answer, or these inputs?
+
+print speed_fraction(16,20)
+# >>> 0.00833333333
